d74dcc655395029dbffe43f19545e6c04a9fe10a
fibergitt <fibergitt@gmail.com>
2022-07-25 05:52:31 +0100

    re-added Boni-garcia, fixed issue with webdriver implementation

--- src/test/java/practice/BaseClass.java
+++ src/test/java/practice/BaseClass.java
@@ -1,24 +1,21 @@
 package practice;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-
-import java.util.concurrent.TimeUnit;
-
 public class BaseClass {
-    static WebDriver driver;
-    public static String URL = "https://instantcrowdfunding.com/home";
+    public static WebDriver driver;
+    static String URL = "https://instantcrowdfunding.com/home";
 
     @BeforeMethod
     static void setUp(){
-        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
-        ChromeOptions options = new ChromeOptions().addArguments("start-fullscreen");
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get(URL);
+        driver.manage().window().maximize();
     }
 
     @AfterMethod
--- src/test/java/practice/PracticeWork.java
+++ src/test/java/practice/PracticeWork.java
@@ -1,13 +1,12 @@
 package practice;
 
 import org.testng.annotations.Test;
-
 import static org.testng.Assert.assertEquals;
 
 public class PracticeWork extends BaseClass {
 
     @Test
-    void verifyPageTitle() {
+    public static void verifyPageTitle() {
         driver.get(URL);
         assertEquals("Instant CrowdFunding", driver.getTitle());
     }
