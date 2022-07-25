919f222ecbb555790c5ab765fe61eb79c1fa95cc
fibergitt <fibergitt@gmail.com>
2022-07-25 13:17:26 +0100

    added more tests

--- src/test/java/practice/PracticeWork.java
+++ src/test/java/practice/PracticeWork.java
@@ -1,6 +1,12 @@
 package practice;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+
+import java.util.List;
+
 import static org.testng.Assert.assertEquals;
 
 public class PracticeWork extends BaseClass {
@@ -9,5 +15,23 @@ public class PracticeWork extends BaseClass {
     public static void verifyPageTitle() {
         driver.get(URL);
         assertEquals("Instant CrowdFunding", driver.getTitle());
+    }
+
+    @Test
+    public void numberOfCampaigns(){
+        List<WebElement> campaigns = driver
+                .findElement(By.cssSelector(".my-2"))
+                .findElements(By.cssSelector(".col-md-4"));
+             System.out.println("Number of Campaigns: " + campaigns.size());
+    }
+
+    @Test
+    public void listCurrentCampaigns(){
+        List<WebElement> campaignTitles = driver
+                .findElement(By.cssSelector(".my-2"))
+                .findElements(By.cssSelector(".card-header"));
+        for ( WebElement e : campaignTitles) {
+            System.out.println(e.getText());
+        }
     }
 }
